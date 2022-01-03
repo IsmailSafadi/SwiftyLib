faa01c315145d56a2c26ba2ad07faca3abace76a
IsmailSafadi <ismailsafadi@gmail.com>
2022-01-03 21:45:02 +0200

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by IsmailSafadi on 03.01.2022.
+//
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
