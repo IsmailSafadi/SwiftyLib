aa6a43c25023c9911bccdc3adcb10db0252144dc
IsmailSafadi <ismailsafadi@gmail.com>
2022-01-03 21:23:55 +0200

    first commit

---
+++ Shared/SwiftyLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExamplesApp.swift
+//  Shared
+//
+//  Created by IsmailSafadi on 03.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
